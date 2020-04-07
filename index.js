ea401cbc3301ad8cc89f5f713962ef3ca7474e49
Eduardo Reis <eduardo4jesus@gmail.com>
2020-04-07 13:29:34 -0400

    Demonstrating Parameter Types. JSON on Express over entire applicaiton.

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
+// app.get('/', (request, response) => {
+app.get('/users', (request, response) => {
   console.log(`Request received`)
-  // response.send('Hello World');
+
+  /* Query Params */
+  console.log(`Query ${request.query}`)
+  console.log('Query ' + request.query)
+  console.log(request.query)
+  console.log(`Query ${JSON.stringify(request.query)}`)
+  console.log(`Query ${request.query['name']}`)
+  response.json({message: 'Hello World'});
+})
+
+app.delete('/users/:id/:age', (request, response) => {
+  console.log(`Request received`)
+
+  /* Route Params */
+  console.log(request.params)
+
+  response.json({message: 'Hello World'});
+})
+
+app.post('/users', (request, response) => {
+  console.log(`Request received`)
+
+  /* 
+   * Body Params. By default returns undefined. 
+   * Enable json on the root of express to interpret it properly through out
+   * the app.
+   */
+  console.log(request.body)
+
   response.json({message: 'Hello World'});
 })
 
