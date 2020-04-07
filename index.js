4c585c9805ee6147790c3949853b860033b53d20
Eduardo Reis <eduardo4jesus@gmail.com>
2020-04-07 12:54:01 -0400

    JSON Nodemon and Scripts

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.get('/', (request, response) => {
   console.log(`Request received`)
-  response.send('Hello World');
+  // response.send('Hello World');
+  response.json({message: 'Hello World'});
 })
 
 app.listen(3333, () => {
